138c60c9f84018e05e1a0e2a7967b816278ebcb7
Noah Schechter <noahschechter7@gmail.com>
2017-04-03 23:05:24 -0400

    Done.

--- components/Board.js
+++ components/Board.js
@@ -4,8 +4,12 @@ const Field = require('./Field');
 class Board extends React.Component {
   render () {
     const { board, onClick } = this.props;
+    let fieldList = board.map((player, i) =>
+    	<Field key={i} player={player} onClick={onClick.bind(null, i)} />
+    );
     return (
-      <div>
+      <div className="board">
+      	{fieldList}
       </div>
     );
   }
--- components/Game.js
+++ components/Game.js
@@ -6,29 +6,51 @@ const solutions = require('./solutions');
 class Game extends React.Component {
   constructor (props) {
     super(props);
-    this.state = {};
+    this.state = { 
+      board: [null, null, null, null, null, null, null, null, null],
+      turn: 'X'
+    };
 
     this.handleReset = this.handleReset.bind(this);
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleReset (ev) {
+    ev.preventDefault();
+    this.setState({
+      board: [null, null, null, null, null, null, null, null, null],
+      turn: 'X'
+    })
   }
 
   handleClick (i, ev) {
+    ev.preventDefault();
+    let tempBoard = this.state.board;
+    tempBoard[i] = this.state.turn === 'X' ? 'X' : 'O'
+    const turn = this.state.turn === 'X' ? 'O' : 'X';
+    this.setState({ board: tempBoard, turn: turn });
   }
 
   getWinner () {
+    let winner = solutions.map(
+      (solution) => solution.map((i) => this.state.board[i]).join('')
+    );
+    winner = winner.find((w) => w === "OOO" || w === "XXX");
+    return winner && winner[0];
   }
 
   isComplete () {
+    return this.state.board.every((field) => field !== null);
   }
 
   render () {
     return (
-      <div>
+      <div className="game">
+        <Board board={this.state.board} onClick={this.handleClick} />
+        {this.isComplete() ? <Status winner={this.getWinner()} /> : null}
+        <button className='game__reset' onClick={this.handleReset}>Reset</button>
       </div>
-    );
+      );
   }
 }
 
