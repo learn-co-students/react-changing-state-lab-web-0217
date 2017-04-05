eb97545846e9fdb555d169b0d87fc5408531fadc
hwalborn <hwalborn@gmail.com>
2017-04-05 16:29:16 -0400

    tics tax toes

--- components/Field.js
+++ components/Field.js
@@ -1,10 +1,20 @@
 const React = require('react');
 
 class Field extends React.Component {
+
+  addClass() {
+    if(this.props.player === undefined){
+      return false
+    } else {
+      return true
+    }
+  }
+
   render () {
     const { player, onClick } = this.props;
     return (
-      <button>
+      <button disabled={this.addClass()} onClick={onClick} className="field">
+        <h1>{player}</h1>
       </button>
     );
   }
--- components/Status.js
+++ components/Status.js
@@ -1,10 +1,22 @@
 const React = require('react');
 
 class Status extends React.Component {
+
+  displayWinner(winner){
+    if(winner === 'X'){
+      return <p>X wins</p>
+    } else if(winner === 'O'){
+      return <p>O wins</p>
+    } else {
+      return <p>Tie</p>
+    }
+  }
+
   render () {
     const { winner } = this.props;
     return (
-      <div>
+      <div className='status'>
+        {this.displayWinner(winner)}
       </div>
     );
   }
