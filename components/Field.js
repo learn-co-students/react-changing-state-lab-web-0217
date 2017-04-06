ec29ae8c750aa43a357ba514d0bcbe74741ed306
github username <github email address>
2017-04-06 14:12:28 -0400

    Done.

--- components/Field.js
+++ components/Field.js
@@ -1,10 +1,22 @@
 const React = require('react');
 
 class Field extends React.Component {
+
   render () {
     const { player, onClick } = this.props;
+
+    let turn
+
+    if(player==='X' || player==='O') {
+      turn = true
+    } else {
+      turn = false
+    }
+
+
     return (
-      <button>
+      <button className="field" onClick={onClick} disabled={turn}>
+        {player}
       </button>
     );
   }
