617bf09bafb9f01d1e5c5ddf6419ba50e178faf8
vrathsmill <vlrathsmill@gmail.com>
2017-04-06 14:12:25 -0400

    Done.

--- components/Board.js
+++ components/Board.js
@@ -5,10 +5,12 @@ class Board extends React.Component {
   render () {
     const { board, onClick } = this.props;
     return (
-      <div>
+      <div className="board">
+        {this.props.board.map((spot, i) => {
+          return <Field value={this.props.board[i]} key={i} onClick={onClick.bind(null, i)}/>
+        })}
       </div>
     );
   }
 }
-
 module.exports = Board;
--- components/Field.js
+++ components/Field.js
@@ -1,13 +1,22 @@
+module.exports = Field;
+
 const React = require('react');
 
 class Field extends React.Component {
+
   render () {
     const { player, onClick } = this.props;
+    let turn
+    if (player==='X' || player==='O') {
+      turn = true
+    } else {
+      turn = false
+    }
     return (
-      <button>
+      <button className="field" onClick={onClick} disabled={turn}>
+        {player}
       </button>
     );
   }
 }
-
 module.exports = Field;
