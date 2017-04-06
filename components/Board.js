c60065f09aea167685c1ba90667b6e64fc91564f
Frederico De Paoli <frederico.de.paoli@gmail.com>
2017-04-06 14:48:09 -0400

    Done.

--- components/Board.js
+++ components/Board.js
@@ -4,8 +4,14 @@ const Field = require('./Field');
 class Board extends React.Component {
   render () {
     const { board, onClick } = this.props;
+    const allFields = board.map((player, i) => {
+        return(
+          <Field key={i} player={player} onClick={onClick.bind(null,i)}/>
+        )
+    })
     return (
-      <div>
+      <div className="board">
+        {allFields}
       </div>
     );
   }
--- components/Field.js
+++ components/Field.js
@@ -3,10 +3,13 @@ const React = require('react');
 class Field extends React.Component {
   render () {
     const { player, onClick } = this.props;
-    return (
-      <button>
-      </button>
-    );
+    let button
+    if (player) {
+      button = <button className="field" onClick={onClick} disabled> {player} </button>
+    } else {
+      button = <button className="field" onClick={onClick} disabled={false}> {player} </button>
+    }
+    return button
   }
 }
 
